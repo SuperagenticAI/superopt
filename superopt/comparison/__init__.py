236744275e60956eb820df214c8e43c1e9f93aca
Shashikant86 <shashikant.jagtap@icloud.com>
2025-12-29 21:44:30 +0000

    Empty Github pages brnach

--- superopt/comparison/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Comparison framework for evaluating SuperOpt against GEPA and ACE."""
-
-from superopt.comparison.ace_comparison import ACEComparison
-from superopt.comparison.framework import ComparisonFramework
-from superopt.comparison.gepa_comparison import GEPAComparison
-
-__all__ = ["GEPAComparison", "ACEComparison", "ComparisonFramework"]
-
